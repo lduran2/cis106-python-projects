5eeaefe8734202ae5e428e8bf075d527396aaf6f
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2020-12-20 02:10:36 -0500

    [final/option4] Parsed level 2 of credit card configuration.

--- final-project/credit_card.py
+++ final-project/credit_card.py
@@ -3,12 +3,14 @@
 Classes and methods for handling credit cards.
 
 By     : Leomar Duran <https://github.com/lduran2>
-When   : 2020-12-20t0148
+When   : 2020-12-20t0208
 Where  : Community College of Philadelphia
 For    : CIS 106/Introduction to Programming
-Version: 1.0
+Version: 1.1
 
 Changelog:
+    v1.1 - 2020-12-20t0208
+        Parsed second level of credit card configuration.
     v1.0 - 2020-12-20t0148
         Parsed top level of credit card configuration.
 '''
@@ -34,10 +36,18 @@ def license(filename='credit-card-config'):
         # for each of the remaining lines
         for line in lines[2:]:
             # if the line is first level,
-            if (not(line[0].startswith('\t'))):
+            if (not(line.startswith('\t'))):
                 # then it is the name of a major industry identifier
-                industries.append({ 'name' : line.strip() })
-            # end if (not(line[0].startswith('\t')))
+                # update the current industry, and append it to the list
+                industry = { 'name' : line.strip(), 'companies' : [] }
+                industries.append(industry)
+            # end if (not(line.startswith('\t')))
+            # if the line is second level,
+            elif (not(line.startswith('\t' * 2))):
+                # then it is a company
+                # append to current industry's companies list
+                industry['companies'].append(line.strip())
+            # end elif (not(line.startswith('\t' * 2)))
         # end for line in lines[2:]
 
         # print the result and its length
